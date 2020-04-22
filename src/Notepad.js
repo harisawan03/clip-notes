aa447f18393400be2ed919463fc4ac675a7cc669
Haris <harisawan03@gmail.com>
2020-04-22 11:40:55 -0500

    add and style note editor and note display

---
+++ src/Notepad.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Notepad.css';
+
+export default class Notepad extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userNotes: ''
+    };
+
+    this.handleUserNotes = this.handleUserNotes.bind(this);
+  }
+
+  handleUserNotes(e) {
+    this.setState ({
+      userNotes: e.target.value
+    });
+  }
+
+  render() {
+    return(
+      <div className="Notepad">
+        <textarea
+          className="Notes-edit"
+          type="text"
+          onChange={this.handleUserNotes}
+          value={this.state.userNotes}        
+        />
+        <p className="Notes-display">
+          {this.state.userNotes}
+        </p>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Notepad from './Notepad'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Notepad />
   </React.StrictMode>,
   document.getElementById('root')
 );
