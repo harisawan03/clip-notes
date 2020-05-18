140406ea0f840aedc04f556838d7209f4bc75585
Haris <harisawan03@gmail.com>
2020-05-18 09:26:06 -0500

    remove unnecessary constructor from Format.js

--- src/Format.js
+++ src/Format.js
@@ -2,10 +2,6 @@ import React from 'react'
 
 // take state of notepad and format it
 export default class Format extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     return(  
       <textarea readOnly
